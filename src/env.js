b930af36fd745a8c887563b804ba174a69f235fb
estiven2111 <estiven.a.martinez@gmail.com>
2023-05-16 10:59:56 -0500

    URL AUTH

--- src/env.js
+++ src/env.js
@@ -2,7 +2,7 @@
 // URL_BACK =  "http://localhost:3001"
 
 // URL_FRONT = "https://urbanclub.club"  
-URL_FRONT = "https://urbanclub.vercel.app"  
+URL_FRONT = "https://www.urbanclub.vercel.app"  
 URL_BACK =  "https://backeste-production.up.railway.app"
 
 module.exports = {URL_FRONT,URL_BACK}
