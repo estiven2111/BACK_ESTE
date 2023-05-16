735e516e1a383724e762c7fc36dcc3cf623aeba9
estiven2111 <estiven.a.martinez@gmail.com>
2023-05-16 08:24:09 -0500

    url front y back_1

--- src/env.js
+++ src/env.js
@@ -2,6 +2,6 @@
 // URL_BACK =  "http://localhost:3001"
 
 URL_FRONT = "urbanclub.club"
-URL_BACK =  "https://backeste-production.up.railway.app/"
+URL_BACK =  "https://backeste-production.up.railway.app"
 
 module.exports = {URL_FRONT,URL_BACK}
