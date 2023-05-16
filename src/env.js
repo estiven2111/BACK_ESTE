20bf2a31407ced7940d7f25d804f4d281ec7d5ac
estiven2111 <estiven.a.martinez@gmail.com>
2023-05-16 14:33:27 -0500

    prueba9

--- src/env.js
+++ src/env.js
@@ -1,8 +1,8 @@
 URL_FRONT = "http://localhost:3000"
-URL_BACK =  "http://localhost:3001"
+// URL_BACK =  "http://localhost:3001"
 
 // URL_FRONT = "https://urbanclub.club"  
 // URL_FRONT = "https://pruebafron.vercel.app"  
-// URL_BACK =  "https://backeste-production.up.railway.app"
+URL_BACK =  "https://backeste-production.up.railway.app"
 
 module.exports = {URL_FRONT,URL_BACK}
