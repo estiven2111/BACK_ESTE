0ff4b69d3a1f915184483e05f4b38036d91ce805
estiven2111 <estiven.a.martinez@gmail.com>
2023-05-16 20:17:58 -0500

    ecommer server16

--- src/middlewares/isAuthGoogle.js
+++ src/middlewares/isAuthGoogle.js
@@ -1,11 +1,40 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const isAuthGoogle = (req, res, next) => {
-    console.log("este es isAutGoogleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",req.user);
-    // res.json({isauthdevuelve:req})
-    if(req.user) {
-        next();
-    } else {
-        res.status(401).json({message: "You are not logged in",user:`el req es ${req.user}`});
+
+    // if(req.user) {
+    //     next();
+    // } else {
+    //     res.status(401).json({message: "You are not logged in",user:`el req es ${req.user}`});
+    // }
+
+    const autHeader = req.get("Authorization");
+   
+    if (autHeader) {
+      //obtener el token
+      const token = autHeader.split(" ")[1];
+      console.log(token);
+     
+      // comprobar el JWT
+      
+      try {
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+  
+          console.log(user);
+
+        //   if(!userById) {
+        //     res.json('error')
+        //   } else {
+        //     req.user = userById
+
+        //   }
+      } catch (error) {
+        console.log(error)
+      
+      }
     }
+  
+      return next()
   }
 
 module.exports = isAuthGoogle
